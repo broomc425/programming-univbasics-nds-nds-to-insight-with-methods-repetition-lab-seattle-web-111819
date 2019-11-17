e81dc36939eafe64f7657ecdf00796b9ef195276
Charlie Broom <broomc425@gmail.com>
2019-11-17 00:16:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -49,14 +49,15 @@ def total_gross(source)
   # return the total
   dir_to_earnings_hash = directors_totals(source)
   dir_names = list_of_directors(source)
-  i = 0
-  
   total = 0
+  
+  i = 0
   while i < dir_names.length do
     dir_name = dir_names[i]
     total += dir_to_earnings_hash(source)
     i += 1
-    
+  end
+  total
 end
 
 
