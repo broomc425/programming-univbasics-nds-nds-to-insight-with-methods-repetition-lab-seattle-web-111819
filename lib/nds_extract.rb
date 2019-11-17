da33cd547800c5bc3ab4cd0d648807710dabbfd8
Charlie Broom <broomc425@gmail.com>
2019-11-17 00:06:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -47,7 +47,7 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  dir_to_earnings
+  dir_to_earnings_hash = directors_totals(source)
 end
 
 
